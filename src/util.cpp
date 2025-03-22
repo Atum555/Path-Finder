5e2c3698d2e94b4d6a4e2731a38671ef7883d3ff
Claudio Meireles <claudiomeireles555@gmail.com>
2025-03-22 16:23:58 +0000

    Add String Trim functions

---
+++ src/util.cpp
@@ -0,0 +1,18 @@
+#include "functions.h"
+
+#include <algorithm>
+
+using namespace std;
+
+void ltrim(string &s) {
+    s.erase(s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); }));
+}
+
+void rtrim(string &s) {
+    s.erase(find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end());
+}
+
+void trim(string &s) {
+    ltrim(s);
+    rtrim(s);
+}

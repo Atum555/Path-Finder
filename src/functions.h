e9d3b4700367e6adf9ae4e9ebd1b34698ab270a5
Claudio Meireles <claudiomeireles555@gmail.com>
2025-03-22 16:24:23 +0000

    Add a Functions.h with all function declarations

---
+++ src/functions.h
@@ -0,0 +1,23 @@
+#ifndef __FUNCTIONS__
+#define __FUNCTIONS__
+
+#include <fstream>
+#include <string>
+
+#include "data_structs/distance.h"
+#include "data_structs/graph.h"
+#include "data_structs/location.h"
+
+void ltrim(std::string &s);
+
+void rtrim(std::string &s);
+
+void trim(std::string &s);
+
+Graph<Location, Distance> *parseDataFiles(std::ifstream &locations, std::ifstream &distances);
+
+Vertex<Location, Distance> *findVertexById(Graph<Location, Distance> *g, int id);
+
+Vertex<Location, Distance> *findVertexByCode(Graph<Location, Distance> *g, const std::string &code);
+
+#endif

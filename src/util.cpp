a6b05692271cb86d2c1130f7a890af71846d5130
Vasco Vieira <up202307797@up.pt>
2025-03-24 10:28:19 +0000

    Add Path Sum operation

--- src/util.cpp
+++ src/util.cpp
@@ -1,6 +1,7 @@
 #include "functions.h"
 
 #include <algorithm>
+#include <stdexcept>
 
 using namespace std;
 
@@ -15,4 +16,23 @@ void rtrim(string &s) {
 void trim(string &s) {
     ltrim(s);
     rtrim(s);
+}
+
+Path &Path::operator+=(const Path &other) {
+    if (this->nodes.empty()) {
+        this->nodes    = other.nodes;
+        this->distance = other.distance;
+        return *this;
+    }
+
+    // Check if paths are compatible
+    if (this->nodes.back() != other.nodes.front()) throw runtime_error("Trying to add not compatible paths!");
+
+    // Merge paths
+    this->nodes.insert(this->nodes.end(), other.nodes.begin() + 1, other.nodes.end());
+
+    // Update distance
+    this->distance += other.distance;
+
+    return *this;
 }
